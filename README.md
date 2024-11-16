1facc0791ef53bbb74c69b53326b40e5ec509a99
DSri Seah <11952933+dsriseah@users.noreply.github.com>
2024-11-15 19:24:55 -0500

    initial build+code placeholder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jamshire-underworld
 submission for New JAMshire 2024
+
+This repo is designed to live in a directory called `sna-underworld` that is inside an unreleased version of the [URSYS](https://github.com/dsriseah/ursys/) monorepo, which is what is providing the `@ursys/core` package through a `file:` URL in `package.json`. Eventually URSYS will be stable and made available as a library.
+
---
+++ app-source/underworld.ts
@@ -0,0 +1,38 @@
+/*///////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+  Main Application Entry Point
+
+  The SNA web app compiler will dynamically bundle any .ts file into the
+  __app_imports.ts file and use it as an entry point for esbuild.
+
+  Since this is the first module to load, we load other SNA components
+  here and initialize them before starting the SNA lifecycle.
+  
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
+
+import { SNA, ConsoleStyler } from '@ursys/core';
+
+/// TYPE DECLARATIONS /////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const LOG = console.log.bind(this);
+const PR = ConsoleStyler('app', 'TagGreen');
+
+/// LIFECYCLE HOOKS ///////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+SNA.Hook('LOAD_CONFIG', () => {});
+
+/// RUNTIME ///////////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+(async () => {
+  LOG(...PR('Initializing UNDERGROUND App'));
+
+  // Set the global configuration object
+  SNA.GlobalConfig({ no_hmr: true, no_urnet: true });
+
+  // After all modules are initialized, start the SNA lifecycle this will
+  // call PreConfig() and PreHook() all all registered modules.
+  await SNA.Start();
+})();
