f42969ec6e66005d775a0a8e622b0278ff3fb24a
DSri Seah <11952933+dsriseah@users.noreply.github.com>
2024-11-18 06:00:05 -0500

    POC points cloud test

--- app-source/client/games/02-test-points.ts
+++ app-source/client/games/02-test-points.ts
@@ -22,9 +22,29 @@ const VISUALS: { [key: string]: THREE.Object3D } = {};
 
 /// LIFECYCLE METHODS /////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function SetupScene() {}
+function SetupScene() {
+  const vertices = [];
+  for (let i = 0; i < 10000; i++) {
+    const x = THREE.MathUtils.randFloatSpread(2000);
+    const y = THREE.MathUtils.randFloatSpread(2000);
+    const z = THREE.MathUtils.randFloatSpread(2000);
+    vertices.push(x, y, z);
+  }
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+  const material = new THREE.PointsMaterial({ color: 0x888888 });
+  const points = new THREE.Points(geometry, material);
+  points.material.size = 3;
+  VISUALS.points = points;
+  Renderer.RP_AddVisual('world', points);
+}
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function Update() {}
+function Update() {
+  const { points } = VISUALS;
+  points.rotation.x += 0.001;
+  points.rotation.y += 0.001;
+  points.rotation.z += 0.001;
+}
 
 /// EXPORTS ///////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
